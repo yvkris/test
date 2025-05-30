fe64448d1bf236277be591ec1403c36c4cc3b10d
Vamsi Krishna <67522305+yvkris@users.noreply.github.com>
2025-05-30 17:48:14 +0000

    Fast API

--- api/index.py
+++ api/index.py
@@ -1,10 +1,7 @@
-from http.server import BaseHTTPRequestHandler
- 
-class handler(BaseHTTPRequestHandler):
- 
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header('Content-type','text/plain')
-        self.end_headers()
-        self.wfile.write('Hello, world!'.encode('utf-8'))
-        return
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"message": "Hello, World!"}
